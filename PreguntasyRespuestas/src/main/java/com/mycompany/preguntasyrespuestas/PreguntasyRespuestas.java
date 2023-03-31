13b05d84b50bf1da2788b4018ba3cc08f86e2558
Frnn4268 <appstutorialesy@gmail.com>
2023-03-30 20:52:58 -0600

    Primer commit

---
+++ PreguntasyRespuestas/src/main/java/com/mycompany/preguntasyrespuestas/PreguntasyRespuestas.java
@@ -0,0 +1,12 @@
+package com.mycompany.preguntasyrespuestas;
+
+/**
+ *
+ * @author Frrn
+ */
+public class PreguntasyRespuestas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
